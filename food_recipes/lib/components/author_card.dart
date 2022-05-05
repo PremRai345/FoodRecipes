ec27e3b3034425d7d94ecc7628303e9eddb667f9
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-05-05 18:34:57 +0545

    List View, Grid View
    List view and grid view is completed.

--- food_recipes/lib/components/author_card.dart
+++ food_recipes/lib/components/author_card.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:food_recipes/screen/circle_image.dart';
 import 'package:food_recipes/theme/theme.dart';
 
+import 'circle_image.dart';
+
 class AuthorCard extends StatefulWidget {
-  // 1
   final String authorName;
   final String title;
   final ImageProvider? imageProvider;
+
   const AuthorCard({
     Key? key,
     required this.authorName,
@@ -15,59 +16,49 @@ class AuthorCard extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AuthorCard> createState() => _AuthorCardState();
+  _AuthorCardState createState() => _AuthorCardState();
 }
 
 class _AuthorCardState extends State<AuthorCard> {
   bool _isFavorited = false;
-  // 2
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace return Container
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              CircleImage(
-                imageProvider: widget.imageProvider,
-                imageRadius: 28,
-              ),
-              // 2
-              const SizedBox(width: 8),
-              // 3
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.authorName,
-                    style: FoodRecipeTheme.darkTextTheme.headline2,
-                  ),
-                  Text(
-                    widget.title,
-                    style: FoodRecipeTheme.darkTextTheme.headline3,
-                  )
-                ],
-              ),
-            ],
-          ),
-          // TODO 2: add IconButton
+          Row(children: [
+            CircleImage(
+              imageProvider: widget.imageProvider,
+              imageRadius: 28,
+            ),
+            const SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.authorName,
+                  style: FoodRecipeTheme.lightTextTheme.headline2,
+                ),
+                Text(
+                  widget.title,
+                  style: FoodRecipeTheme.lightTextTheme.headline3,
+                )
+              ],
+            ),
+          ]),
           IconButton(
-            // 1
             icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
             iconSize: 30,
-            // 2
             color: Colors.red[400],
             onPressed: () {
-              // 3
               setState(() {
                 _isFavorited = !_isFavorited;
               });
             },
-          )
+          ),
         ],
       ),
     );
